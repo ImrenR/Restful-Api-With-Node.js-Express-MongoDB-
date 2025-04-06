f4370be89eefa1c09e99c63c74fe8fa448ac5a0d
ImrenR <imrenrahba@gmail.com>
2025-04-06 18:58:01 +0200

    up

--- routes/posts.js
+++ routes/posts.js
@@ -1,12 +1,11 @@
 const express = require('express');
-
 const router = express.Router();
-
+const Post = require("../models/Post");
 // ROUTES
 router.get('/posts', (req,res)=>{
 
-  res.send("We are on home")
+res.send("We are on home")
   
-  });
+});
   
   module.exports =router ;
