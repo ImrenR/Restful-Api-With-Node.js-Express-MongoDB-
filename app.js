4cf4bec933530cf8428524b8382d3f1adfcc8d49
ImrenR <imrenrahba@gmail.com>
2025-04-06 18:21:28 +0200

    UP

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 "use stric";
 
 const express = require ("express");
-
 const app = express();
+const mongoose = require ("mongoose");
 
 
 // ROUTES
@@ -13,6 +13,18 @@ res.send("We are on home")
 });
 
 
+// CONNECT TO DB
+
+async function startServer() {
+  try {
+ await mongoose.connect("mongodb+srv://imrenrahbay:cfI0CJJjbUyVG9uj@imrenr.citbvbk.mongodb.net/") ;
+ console.log('Connected to DB!');
+  } catch (error) {
+    console.error('Error connecting to DB:', error);
+  }
+  
+}
+startServer();
 
 
 
