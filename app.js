a73c0d28bfc547ec5a014be1a28b6d1ee6be7729
ImrenR <imrenrahba@gmail.com>
2025-04-06 17:47:46 +0200

    up

---
+++ app.js
@@ -0,0 +1,18 @@
+"use stric";
+
+const express = require ("express");
+
+const app = express();
+
+// ROUTES
+app.get('/', (req,res)=>{
+
+res.send("We are on home")
+
+})
+
+
+
+
+
+app.listen (8000, ()=> { console.log ("Server is running on port 8000")});
